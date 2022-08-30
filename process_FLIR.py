7ff8e266189e0b4358a4c31f629fbcc94159f97d
SvenPaterson <stephenjgarden@icloud.com>
2022-08-30 11:37:30 -0400

    FLIR file save, error handling, multi torque files

--- process_FLIR.py
+++ process_FLIR.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import pandas as pd
+import pickle
 
 from PIL import Image, ImageOps
 from pytesseract import pytesseract
@@ -9,76 +10,77 @@ from pytesseract import pytesseract
 pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
-def get_FLIR_data(raw_data_path, crop_data_path, output=''):
-    file_list = [f for f in os.listdir(raw_data_path) if f.endswith(".jpg")]
+def get_FLIR_data(raw_data_path, crop_data_path):
+    file_list = [f for f in os.listdir(raw_data_path) if f.endswith('.jpg')]
     if not len(file_list):
-        raise ValueError("There are no FLIR img files available to process")
-    results = {"timestamp": [], "temp": []}
+        raise ValueError('There are no FLIR img files available to process')
+    results = {'timestamp': [], 'temp': []}
     fail_list = []
-    for i, file in enumerate(file_list):
-        print('\n', file)
-        img = Image.open(os.path.join(raw_data_path, file))
 
-        # get exif date_time string from jpeg file, produced by FLIR camera
-        img_exif = img.getexif()
-        for key, val in img_exif.items():
-            if key == 306:  # exif {key: '306', val: 'datetime'}
-                date_time = val
+    prev_data = os.path.join(raw_data_path, file_list[0].removesuffix('.jpg'))
+    
+    try:
+        print(prev_data)
+        df = pickle.load(open(f'{prev_data}.pickle', 'rb'))
 
-        # crop, resize, resample, convert to greyscale, invert image
-        img = img.crop((30, 5, 125, 24))
-        w, h = img.size
-        scale = 10
-        new_size = w*scale, h*scale
-        img = img.resize(new_size, Image.Resampling.LANCZOS)
-        img = img.convert('L')
-        img = ImageOps.invert(img)
-        img = img.point(lambda x: 0 if x < 155 else 255, '1')
+    except:
+        print("Initial image processing required, please wait...")
+        for i, file in enumerate(file_list):
+            img = Image.open(os.path.join(raw_data_path, file))
 
-        # save cropped greyscale image
-        file_name = f"{file.split('.')[0]}_cropped.jpg"
-        crop_path = os.path.join(crop_data_path, file_name)
-        img.save(crop_path)
+            # get exif date_time string from jpeg file, produced by FLIR camera
+            img_exif = img.getexif()
+            for key, val in img_exif.items():
+                if key == 306:  # exif {key: '306', val: 'datetime'}
+                    date_time = val
 
-        # perform OCR on cropped image
-        string = pytesseract.image_to_string(img, config='--psm 6')
+            # crop, resize, resample, convert to greyscale, invert image
+            img = img.crop((30, 5, 125, 24))
+            w, h = img.size
+            scale = 10
+            new_size = w*scale, h*scale
+            img = img.resize(new_size, Image.Resampling.LANCZOS)
+            img = img.convert('L')
+            img = ImageOps.invert(img)
+            img = img.point(lambda x: 0 if x < 155 else 255, '1')
 
-        # interpet OCR output and save data
-        if string[2] == '.':
-            try:
-                t = float(string[:4])
-                results['temp'].append(t)
-                results["timestamp"].append(date_time)
-                print('temp read as: ', string[:4], 'degF')
-            except ValueError:
-                fail_list.append(file)
-                print(string[:4], 'failed')
+            # save cropped greyscale image
+            file_name = f'{file.split(".")[0]}_cropped.jpg'
+            crop_path = os.path.join(crop_data_path, file_name)
+            img.save(crop_path)
+
+            # perform OCR on cropped image
+            string = pytesseract.image_to_string(img, config='--psm 6')
+
+            # interpet OCR output and save data
+            if string[2] == '.':
+                try:
+                    t = float(string[:4])
+                    results['temp'].append(t)
+                    results['timestamp'].append(date_time)
+                    print('temp read as: ', string[:4], 'degF')
+                except ValueError:
+                    fail_list.append(file)
+                    print(string[:4], 'failed')
+            else:
+                try:
+                    t = float(string[:3])
+                    results['temp'].append(t)
+                    results['timestamp'].append(date_time)
+                except ValueError:
+                    fail_list.append(file)
+                    print(string[:3], 'failed')
+
+        if fail_list:
+            print('\nOCR failed on the following files:\n', fail_list)
+
         else:
-            try:
-                t = float(string[:3])
-                results['temp'].append(t)
-                results['timestamp'].append(date_time)
-                print('temp read as: ', string[:3], 'degF')
-            except ValueError:
-                fail_list.append(file)
-                print(string[:3], 'failed')
-
-    if fail_list:
-        print('\nOCR failed on the following files:\n', fail_list)
-
-    if output == 'csv':
-        with open('results.csv', 'w', newline='') as f:
-            w = csv.writer(f)
-            w.writerow(results.keys())
-
-            timestamp, temp = tuple(results.values())
-            for i, ts in enumerate(timestamp):
-                w.writerow([ts, temp[i]])
-    else:
-        df = pd.DataFrame(results)
-        df.timestamp = pd.to_datetime(df.timestamp,
-                                      format='%Y:%m:%d %H:%M:%S')
-        df.rename(columns={"timestamp": "Date_Time",
-                           "temp": "Temperature, degF"},
-                  inplace=True)
-        return df
+            df = pd.DataFrame(results)
+            df.timestamp = pd.to_datetime(df.timestamp,
+                                        format='%Y:%m:%d %H:%M:%S')
+            df.rename(columns={'timestamp': 'Date_Time',
+                               'temp': 'Temperature, degF'},
+                    inplace=True)
+            df.to_pickle(os.path.join(raw_data_path,(prev_data)+'.pickle'))
+    
+    return df
--- process_futek.py
+++ process_futek.py
@@ -5,34 +5,52 @@ import pandas as pd
 from datetime import timedelta
 
 
+def torque_file_to_df(file, sep):
+    # read sample rate of data file
+    with open(file) as f:
+        reader = csv.reader(f)
+        for i in range(21):
+            next(reader)
+        row = next(reader)
+        if sep == ',':
+            sample_rate = int(row[1])
+        if sep == '\t':
+            sample_rate = int(row[0].split('"')[1])
+        
+    df = pd.read_csv(file, sep=sep,
+                    header=23,
+                    index_col='Sample Number',
+                    parse_dates=[['Date', 'Time']])
+    df.drop(columns=df.columns[-1], axis=1, inplace=True)
+    df.rename(columns={"Tracking Value": "Torque, Nm"}, inplace=True)
+
+    # ensure sample rate is added to date_time column
+    trigger_time = df.iloc[0]['Date_Time']
+    time_delta = timedelta(milliseconds=1000/sample_rate)
+    total_time = [trigger_time+i*time_delta for i in range(0, df.shape[0])]
+    df['Date_Time'] = pd.Series(total_time)
+    
+    return df
+
+
 def get_torque_data(raw_data_path):
-    file_list = [f for f in os.listdir(raw_data_path) if f.endswith(".txt")]
+    all_files = os.listdir(raw_data_path)
+    file_list = [f for f in all_files if f.endswith((".txt", ".csv"))]
     if not len(file_list):
         raise ValueError("There is no torque data file available to process")
-    if len(file_list) > 1:
-        raise ValueError("This script cannot process more than one torque data"
-                         " file at a time")
-    else:
-        file = os.path.join(raw_data_path, file_list[0])
-
-        # read sample rate of data file
-        with open(file) as f:
-            reader = csv.reader(f)
-            for i in range(21):
-                next(reader)
-            sample_rate = int(next(reader)[0].split('"')[1])
-
-        # import data file
-        df = pd.read_csv(file, sep='\t',
-                         header=23,
-                         index_col='Sample Number',
-                         parse_dates=[['Date', 'Time']])
-        df.drop(columns=df.columns[-1], axis=1, inplace=True)
-        df.rename(columns={"Tracking Value": "Torque, Nm"}, inplace=True)
-
-        # ensure sample rate is added to date_time column
-        trigger_time = df.iloc[0]['Date_Time']
-        time_delta = timedelta(milliseconds=1000/sample_rate)
-        total_time = [trigger_time+i*time_delta for i in range(0, df.shape[0])]
-        df['Date_Time'] = pd.Series(total_time)
+    for file_path in file_list:
+        file = os.path.join(raw_data_path, file_path)
+        if file_path.endswith(".txt"):
+            sep = '\t'
+        if file_path.endswith(".csv"):
+            sep = ','
+        if not file_path.endswith(".csv") and not file_path.endswith('.txt'): 
+            raise ValueError("Incorrect file type!")
+        if 'df' not in locals():
+            df = torque_file_to_df(file, sep)
+        else:
+            next_df = torque_file_to_df(file, sep)
+            df= pd.concat([df, next_df], ignore_index=True)
+    df.sort_values(by=['Date_Time'], ignore_index=True, inplace=True)
+    print(df)
     return df
