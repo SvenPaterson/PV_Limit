ee82913e1529d1f533ea21c77ae638ac54ad355d
SvenPaterson <stephenjgarden@icloud.com>
2023-07-10 13:24:46 -0400

    remove unused files

--- flir_img_extr.py
+++
@@ -1,7 +0,0 @@
-from exiftool import ExifToolHelper
-
-with ExifToolHelper() as et:
-    for d in et.get_metadata("test_img.jpg"):
-        for k, v in d.items():
-            print(f"Dict: {k} = {v}")
-    et.execute('-b', "test_img.jpg")
